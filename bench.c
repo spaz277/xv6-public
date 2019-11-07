e9693456be835e4559032d9e209b6a55d8865e93
Saransh Rajput <47175720+spaz277@users.noreply.github.com>
2019-11-07 23:48:51 +0530

    Add files via upload

--- bench.c
+++ bench.c
@@ -5,13 +5,16 @@
 
 int main(int argc, char *argv[])
 {
+    
+    double z,x=0;
     int id = fork();
     if (id == 0) {
-        double z, d = 1, x = 0;
-        for (int i = 0; i < 5; ++i) {
-            for (int j = 0; j < 10; ++j) {
-                for (z = 0; z < 8000000; z += d) {
-                    x = x + 3.14 * 89.64; // useless calcs
+        for (int i=0;i<5;++i)
+         {
+            for (int j=0;j<10;++j) 
+            {
+                for (z =0;z <10000000;z++) {
+                    x+=3.14*89.64*198.67; // useless calculations
                 }
             }
         }

df0f45f581bb030782cc738e395d67eb6316e4bd
spaz277 <saransh.rajput@research.iiit.ac.in>
2019-11-07 10:21:56 +0530

    trap changed

--- trap.c
+++ trap.c
@@ -107,13 +107,15 @@ trap(struct trapframe *tf)
   // until it gets to the regular system call return.)
   if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
     exit();
+  #ifdef FCFS//non yielding
+  #else
 
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
   if(myproc() && myproc()->state == RUNNING &&
      tf->trapno == T_IRQ0+IRQ_TIMER)
     yield();
-
+  #endif
   // Check if the process has been killed since we yielded
   if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
     exit();
